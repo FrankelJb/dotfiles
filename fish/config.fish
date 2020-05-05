20c8082503cd6aa479710df3c8dc4b317b1fdc6d
Jonathan Frankel <frankeljb@gmail.com>
2020-05-05 09:33:00 +0200

    Added i3 directory

--- fish/config.fish
+++ fish/config.fish
@@ -1,12 +1,17 @@
 if command -v exa > /dev/null
-	abbr -a l 'exa'
-	abbr -a ls 'exa'
-	abbr -a ll 'exa -l'
-	abbr -a lll 'exa -la'
+	abbr -a l "exa"
+	abbr -a ls "exa"
+	abbr -a ll "exa -l"
+	abbr -a lll "exa -la"
 else
-	abbr -a l 'ls'
-	abbr -a ll 'ls -l'
-	abbr -a lll 'ls -la'
+	abbr -a l "ls"
+	abbr -a ll "ls -l"
+	abbr -a lll "ls -la"
+end
+
+if command -v bat > /dev/null
+	alias cat "bat"
+	alias less "bat"
 end
 
 
--- polybar/launch.sh
+++ polybar/launch.sh
@@ -6,4 +6,5 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-exec polybar --reload main
+echo "---" | tee -a /tmp/polybar.log
+exec polybar --reload main >>/tmp/polybar.log 2>&1 &
